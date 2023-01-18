3a1d33c6dd425cef2e8f34db099802542cd412ae
Lazarina Stoy <78483428+lazarinastoy@users.noreply.github.com>
2023-01-18 15:53:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,13 +16,13 @@ def is_page_indexed(url):
 def main():
     st.set_page_config(page_title="URL Index Checker", page_icon=":guardsman:", layout="wide")
     st.title("URL Index Checker")
-    st.markdown("Upload a CSV file containing a list of URLs to check the indexation status. Note, the column containing the URLs should be titled 'url'")
+    st.markdown("Upload a CSV file containing a list of URLs to check the indexation status.")
     uploaded_file = st.file_uploader("Upload a CSV file containing a list of URLs", type=["csv"])
 
     if uploaded_file is not None:
         try:
             uploaded_file.seek(0)
-            df = pd.read_csv(io.StringIO(uploaded_file.read().decode('utf-8')))
+            df = pd.read_csv(io.StringIO(uploaded_file.decode('utf-8')))
             total_urls = len(df)
             start_time = time.time()
             df['Indexation Status'] = df['url'].apply(is_page_indexed)
@@ -35,24 +35,11 @@ def main():
             buffer.seek(0)
             st.markdown('Download the csv')
             st.markdown("""
-            <form action="data
-            :text/csv;charset=utf-8,%EF%BB%BF" method="post" enctype="text/csv" id="csv_download_form">
+            <form action="data:text/csv;charset=utf-8,%EF%BB%BF" method="post" enctype="text/csv" id="csv_download_form">
                 <input type="hidden" name="file" value='{file}'/>
                 <input type="submit" value="Download" style="display: none" id="csv_download_button"/>
             </form>""".format(file=buffer.getvalue()), unsafe_allow_html=True)
             st.markdown("""<button onclick="document.getElementById('csv_download_button').click();" class="btn btn-primary">Download CSV</button>""", unsafe_allow_html=True)
-            st.markdown("""
-                <script>
-                document.getElementById('csv_download_form').addEventListener('submit', function(event) {
-                    event.preventDefault();
-                    var form = event.target;
-                    var a = document.createElement('a');
-                    a.href = form.action + form.elements.file.value;
-                    a.download = 'data.csv';
-                    a.click();
-                });
-                </script>
-            """, unsafe_allow_html=True)
             indexed_count = indexation_count["URL is Indexed"]
             not_indexed_count = indexation_count["URL is Not Indexed"]
             total = indexed_count + not_indexed_count
@@ -79,3 +66,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
