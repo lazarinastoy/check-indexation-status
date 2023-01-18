9de4755059480207acf748db7e14871ab5592f2e
Lazarina Stoy <78483428+lazarinastoy@users.noreply.github.com>
2023-01-18 15:29:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,19 +17,19 @@ def main():
     uploaded_file = st.file_uploader("Upload a CSV file containing a list of URLs, that has a header with the title 'url'", type=["csv"])
 
     if uploaded_file is not None:
-    try:
-        uploaded_file.seek(0)
-        df = pd.read_csv(io.StringIO(uploaded_file.read().decode('utf-8')))
-        total_urls = len(df)
-        start_time = time.time()
-        for i, row in df.iterrows():
-            st.write(f"{row['url']}: {is_page_indexed(row['url'])}")
-            progress = (i+1) / total_urls
-            remaining_time = ((time.time() - start_time) / (i+1)) * (total_urls - (i+1))
-            st.progress(progress)
-            st.write(f"Time remaining: {round(remaining_time, 2)} seconds")
-    except:
-        st.write("Invalid file format")
+        try:
+            uploaded_file.seek(0)
+            df = pd.read_csv(io.StringIO(uploaded_file.read().decode('utf-8')))
+            total_urls = len(df)
+            start_time = time.time()
+            for i, row in df.iterrows():
+                st.write(f"{row['url']}: {is_page_indexed(row['url'])}")
+                progress = (i+1) / total_urls
+                remaining_time = ((time.time() - start_time) / (i+1)) * (total_urls - (i+1))
+                st.progress(progress)
+                st.write(f"Time remaining: {round(remaining_time, 2)} seconds")
+        except:
+            st.write("Invalid file format")
         
 if __name__ == "__main__":
     main()
